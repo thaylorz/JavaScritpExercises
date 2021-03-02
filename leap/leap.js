ec7a654ca42f0e472a6e80152bfcada144caa10c
thaylorz <thaylor5michi@hotmail.com.br>
2021-03-02 12:10:07 -0300

    Exercise is leap year

---
+++ leap/leap.js
@@ -0,0 +1,7 @@
+//
+// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isLeap = (year) => year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+
